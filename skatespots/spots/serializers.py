7d67ce4ec840e743a3e989a871641c028588a98c
Taneli Riihimäki <m3034@student.jamk.fi>
2019-06-13 18:50:56 +0300

    Migrate and add serializer

---
+++ skatespots/spots/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from spots.models import Spot
+
+# Spot Serializer
+class SpotSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Spot
+        fields = '__all__'
+        
